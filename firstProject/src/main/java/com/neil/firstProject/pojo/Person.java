e85acba5f2ad8007cece63feead100ba74c65fb5
Neil <n@163.com>
2017-03-08 22:12:14 +0800

    c

--- firstProject/src/main/java/com/neil/firstProject/pojo/Person.java
+++ firstProject/src/main/java/com/neil/firstProject/pojo/Person.java
@@ -1,5 +1,16 @@
 package com.neil.firstProject.pojo;
 
 public class Person {
+	
+	private int sex;
 
+	public int getSex() {
+		return sex;
+	}
+
+	public void setSex(int sex) {
+		this.sex = sex;
+	}
+	
+	
 }
